98d6e8701e803ba71841517b470785baa150859f
TedyonGit <doigameri00@gmail.com>
2021-05-11 23:06:54 +0200

    Update index.js

--- index.js
+++ index.js
@@ -69,7 +69,6 @@ app.get('/getAcces', (req, res) => {
 	db.collection('Accounts').doc(req.get('content')).get().then(q => 
 	{
 		if(q.exists) {
-			console.log(Crypt.AES.encrypt(req.get('content'), hash).toString())
 			res.send(Crypt.AES.encrypt(req.get('content'), hash).toString())
 			res.end();
 		} else {
