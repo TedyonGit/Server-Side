7a30163ae08b57a39e5a785a6ff96430db771048
TedyonGit <doigameri00@gmail.com>
2021-05-11 23:59:50 +0200

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const app = express();
 const port = 8080
 const Crypt = require('crypto-js')
 const fetch = require('node-fetch')
+const axios = require('axios')
 
 let hash = "4b6c4d9d79d55456a50b98de2587d2eb9e51c57a60c7ef15344dc974fc69ed9b" // ElectronAPPtedyv2ezpez
 
