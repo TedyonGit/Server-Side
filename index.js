97404195bf5e58731ebb33654cebc3a1f5c65f0a
TedyonGit <doigameri00@gmail.com>
2021-05-23 21:54:38 +0200

    Update index.js

--- index.js
+++ index.js
@@ -140,7 +140,7 @@ function GetWars(host)
 			{
 				let date = data[i+2].split('\n')[1].slice(29, data[i+2].split('\n')[1].length-29)//.slice(24, data[i+2].length-3);
 				let timp = data[i+1].slice(24, data[i+1].length-3);
-				console.log(date, timp)
+				console.log(`Date: ${date}\nTimp: ${timp}`)
 			}
 		}
 	})
